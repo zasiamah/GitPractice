5c18d5c3f2250ac5b3c95f82ab2370623626ed4f
zephania asiamah <zasiamah@yahoo.com>
2022-01-04 19:40:29 -0500

    forth commit

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,9 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("First");
+        System.out.println("second");
+        System.out.println("third");
+        System.out.println("forth");
+
     }
 }
